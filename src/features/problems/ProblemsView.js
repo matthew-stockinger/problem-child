6a71bf0e5fe3a190379813f47d81488d0b3438b0
Matthew Stockinger <matt@matthewstockinger.com>
2023-03-10 08:51:08 -0600

    format problem css

--- src/features/problems/ProblemsView.js
+++ src/features/problems/ProblemsView.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./problemsView.css";
 
 const ProblemsView = () => {
   const [problemsArray, setProblemsArray] = useState([
@@ -8,13 +9,16 @@ const ProblemsView = () => {
 
   return (
     <div className="container">
-      <div className="row">
-        <div className="col"></div>
-        <div className="col">{problemsArray[0].operand1}</div>
-      </div>
-      <div className="row">
-        <div className="col">{problemsArray[0].operation}</div>
-        <div className="col">{problemsArray[0].operand2}</div>
+      <div className="row my-4">
+        <div className="col-2">
+          <div className="problemBox">
+            <p className="h3 text-end">{problemsArray[0].operand1}</p>
+            <p className="h3 text-end">
+              {problemsArray[0].operation} {problemsArray[0].operand2}
+            </p>
+            <hr />
+          </div>
+        </div>
       </div>
     </div>
   );
