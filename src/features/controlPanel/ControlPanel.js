0dc46ee2ed4f384d07e745f054ee3acc102e6a37
Matthew Stockinger <matt@matthewstockinger.com>
2023-03-18 16:49:30 -0500

    adding form validation

--- src/features/controlPanel/ControlPanel.js
+++ src/features/controlPanel/ControlPanel.js
@@ -14,51 +14,27 @@ const ControlPanel = ({ state, stateSetters }) => {
     setShuffle,
   } = stateSetters;
 
-  const whichConstraintsAreInvalid = () => {
-    // return a list of constraints (string names) that are invalid.
-  };
-
   const handleSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
-    // console.log("e.target = ");
-    // console.log(e.target);
     const formdata = new FormData(form);
-    // console.log("formdata = ");
-    // console.log(formdata);
-    console.log(
-      `formdata.get("numberOfProblemsInput") = ${formdata.get(
-        "numberOfProblemsInput"
-      )}`
-    );
-    if (parseInt(formdata.get("numberOfProblemsInput")) < 15) {
-      console.log("inside < 15");
-      form
-        .querySelector("#numberOfProblemsInput")
-        .setCustomValidity("less than 15");
-      form.querySelector("#numberOfProblemsInput").reportValidity();
+    const numProbsValue = parseInt(formdata.get("numberOfProblemsInput"));
+    const numProbsInputElt = form.querySelector("#numberOfProblemsInput");
+    if (numProbsValue < 15) {
+      numProbsInputElt.setCustomValidity("custom validity: less than 15");
     } else {
-      console.log("else");
-      form.querySelector("#numberOfProblemsInput").setCustomValidity("");
-      setNumberOfProblems(parseInt(formdata.get("numberOfProblemsInput")));
+      numProbsInputElt.setCustomValidity("");
+      setNumberOfProblems(numProbsValue);
     }
 
     setShuffle(formdata.get("shuffleCheckbox") === "shuffle" ? true : false);
-    // TODO: update Constraints to be uncontrolled inputs.
     // TODO: add setter calls for Constraints.
-
-    // for (const key of formdata.keys()) {
-    //   console.log(key);
-    // }
-    // for (const value of formdata.values()) {
-    //   console.log(typeof value);
-    // }
   };
 
   return (
     <div className="card">
       <div className="card-body">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="was-validated" noValidate>
           <div className="row justify-content-center">
             <div className="col-md-3">
               <div className="mb-3">
--- src/features/controlPanel/NumberOfProblems.js
+++ src/features/controlPanel/NumberOfProblems.js
@@ -5,22 +5,25 @@ const NumberOfProblems = ({ numberOfProblems }) => {
   const numberOfProblemsInput = React.useRef(null);
 
   return (
-    <label htmlFor="numberOfProblemsInput" className="form-label">
-      Number of Problems:
-      <input
-        id="numberOfProblemsInput"
-        name="numberOfProblemsInput"
-        type="number"
-        className="form-control"
-        defaultValue={numberOfProblems}
-        step="6"
-        min="6"
-        max="60"
-        ref={numberOfProblemsInput}
-        onChange={() => numberOfProblemsInput.current.setCustomValidity("")}
-        required
-      />
-    </label>
+    <>
+      <label htmlFor="numberOfProblemsInput" className="form-label">
+        Number of Problems:
+        <input
+          id="numberOfProblemsInput"
+          name="numberOfProblemsInput"
+          type="number"
+          className="form-control"
+          defaultValue={numberOfProblems}
+          step="6"
+          min="6"
+          max="60"
+          ref={numberOfProblemsInput}
+          required
+        />
+        <div className="valid-feedback">Valid input div</div>
+        <div className="invalid-feedback">Invalid input div</div>
+      </label>
+    </>
   );
 };
 
